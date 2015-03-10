55b6a1a9721fd5dd4ec85a27840694db549673a3
Henning Leutz <leutz@pcsg.de>
2015-03-10 15:38:05 +0100

    helios -> layouts

--- index.php
+++ index.php
@@ -5,3 +5,30 @@
  */
 
 \QUI\Utils\Site::setRecursivAttribute( $Site, 'image_emotion' );
+
+
+/**
+ * Helios body class
+ */
+
+$bodyClass = '';
+
+switch ( $Template->getLayoutType() )
+{
+    case 'layout/startpage':
+        $bodyClass = 'homepage';
+    break;
+
+    case 'layout/leftSidebar':
+        $bodyClass = 'left-sidebar';
+    break;
+
+    case 'layout/rightSidebar':
+        $bodyClass = 'right-sidebar';
+    break;
+
+    default:
+        $bodyClass = 'no-sidebar';
+}
+
+$Engine->assign( 'bodyClass', $bodyClass );
