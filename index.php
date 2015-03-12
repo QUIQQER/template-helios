b31dc531069dde1a1edce4007680d2c255553709
Henning Leutz <leutz@pcsg.de>
2015-03-12 15:45:42 +0100

    template settings

--- index.php
+++ index.php
@@ -6,6 +6,49 @@
 
 \QUI\Utils\Site::setRecursivAttribute( $Site, 'image_emotion' );
 
+/**
+ * colors
+ */
+
+$footerColor  = '#2b252c';
+$controlColor = '#ef8376';
+
+if ( $Project->getConfig( 'templateHelios.settings.controlColor' ) ) {
+    $controlColor = $Project->getConfig( 'templateHelios.settings.controlColor' );
+}
+
+if ( $Project->getConfig( 'templateHelios.settings.footerColor' ) ) {
+    $footerColor = $Project->getConfig( 'templateHelios.settings.footerColor' );
+}
+
+$Engine->assign(array(
+    'controlColor' => $controlColor,
+    'footerColor'  => $footerColor
+));
+
+/**
+ * Header
+ */
+
+$headerText = $Project->firstChild()->getAttribute( 'title' );
+$headerLogo = false;
+
+$confLogoSetting = $Project->getConfig( 'templateHelios.settings.headerLogo' );
+
+if ( $Project->getConfig( 'templateHelios.settings.headerText' ) ) {
+    $headerText = $Project->getConfig( 'templateHelios.settings.headerText' );
+}
+
+if ( $confLogoSetting &&
+     QUI\Projects\Media\Utils::isMediaUrl( $confLogoSetting ) )
+{
+    $headerLogo = $confLogoSetting;
+}
+
+$Engine->assign(array(
+    'headerText' => $headerText,
+    'headerLogo' => $headerLogo
+));
 
 /**
  * Helios body class
