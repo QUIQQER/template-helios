8ab5b78b11cb7ad3727691a8d226a19ef753d247
Henning Leutz <leutz@pcsg.de>
2015-10-31 08:02:57 +0100

    type css class

--- index.php
+++ index.php
@@ -117,5 +117,5 @@ $Engine->assign('bodyClass', $bodyClass);
 
 $Engine->assign(
     'typeClass',
-    'type-'. str_replace(array('/',':'), '', $Site->getAttribute('type'))
+    'type-'. str_replace(array('/',':'), '-', $Site->getAttribute('type'))
 );
